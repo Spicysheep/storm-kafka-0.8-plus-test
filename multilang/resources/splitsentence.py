f2ba8764fc3be00f398be715980ab7eb5325ee86
Spicysheep <rjzotti@gmail.com>
2015-04-21 11:48:24 -0400

    Update splitsentence.py

--- multilang/resources/splitsentence.py
+++ multilang/resources/splitsentence.py
@@ -3,6 +3,9 @@ from decimal import Decimal, getcontext
 getcontext().prec = 12 # Sets the decimal place
 from math import log, exp
 
+import json
+from subprocess import Popen, PIPE
+
 coeffs = {}
 coeffs["Bankruptcy_Index"] = Decimal("-0.00422064")
 coeffs["RE_Scurd_Indicator"] = Decimal("-0.64315405")
@@ -14,19 +17,17 @@ coeffs["Curr_PD_Days_Cnt_S"] = Decimal("1.20648812")
 coeffs["Pay_Idx_100_Indicator"] = Decimal("0")
 intercept = Decimal("1.50227124")
 
-def calculate_score(line):
-    line = line.strip() # Remove whitespace
-    line = line.split(",")
-    ent_cust_id = line[0]
+def calculate_score(model_input):
+    ent_cust_id = model_input["Account_Id"]
     logit = intercept + \
-        Decimal(line[1]) * coeffs["Bankruptcy_Index"] + \
-        Decimal(line[2]) * coeffs["RE_Scurd_Indicator"] + \
-        Decimal(line[3]) * coeffs["FICO_Score"] + \
-        Decimal(line[4]) * coeffs["LOC_Utlz_Scale"] + \
-        Decimal(line[5]) * coeffs["Deposit_Bal_Tier"] + \
-        Decimal(line[6]) * coeffs["Deposit_Bal_Trend"] + \
-        Decimal(line[7]) * coeffs["Curr_PD_Days_Cnt_S"] + \
-        Decimal(line[8]) * coeffs["Pay_Idx_100_Indicator"]
+        Decimal(str(model_input["Bankruptcy_Index"])) * coeffs["Bankruptcy_Index"] + \
+        Decimal(str(model_input["RE_Scurd_Indicator"])) * coeffs["RE_Scurd_Indicator"] + \
+        Decimal(str(model_input["FICO_Score"])) * coeffs["FICO_Score"] + \
+        Decimal(str(model_input["LOC_Utlz_Scale"])) * coeffs["LOC_Utlz_Scale"] + \
+        Decimal(str(model_input["Deposit_Bal_Tier"])) * coeffs["Deposit_Bal_Tier"] + \
+        Decimal(str(model_input["Deposit_Bal_Trend"])) * coeffs["Deposit_Bal_Trend"] + \
+        Decimal(str(model_input["Curr_PD_Days_Cnt_S"])) * coeffs["Curr_PD_Days_Cnt_S"] + \
+        Decimal(str(model_input["Pay_Idx_100_Indicator"])) * coeffs["Pay_Idx_100_Indicator"]
     # Downsample adjustment only for ROR
     adjustment = Decimal(str(log(0.51578631)))
     logit = logit + adjustment
@@ -38,10 +39,14 @@ def calculate_score(line):
     return score
 
 class SplitSentenceBolt(storm.BasicBolt):
-    
+
     def process(self, tup):
-        line = tup.values[0]
-        score = calculate_score(line)
+        acct_id = tup.values[0]
+        p = Popen(['curl http://10.94.98.77:19237/v1/ror?acct_id='+acct_id],stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+        output, err = p.communicate()
+        rc = p.returncode
+        model_input = json.loads(output)
+        score = calculate_score(model_input)
         storm.emit([score])
-        
+
 SplitSentenceBolt().run()
